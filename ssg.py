902a28c4de6b1ccff1bfa251343a037bde4715c6
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-03-30 11:04:38 -0700

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -2,9 +2,10 @@ import typer
 from ssg.site import Site
 
 def main(source, dest):
-    source = 'content'
-    dest = 'dist'
+    source = "content"
+    dest = "dist"
     config = {
-        'source': source,
-        'dest': dest
+        "source": source,
+        "dest": dest
     }
+    class Site(source, dest):
