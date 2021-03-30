49ca8c59be7905a4ed7cb5d7d56a450ba31175d3
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-03-30 10:39:52 -0700

    Update site.py

---
+++ ssg/site.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+class Site():
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents = True, exist_ok = True)
+    def build(self):
+        self.dest.mkdir(parents = True, exist_ok= True)
+        for path in self.source.rglob("*"):
+            if path.isdir(path) == True:
+                path.create_dir()
