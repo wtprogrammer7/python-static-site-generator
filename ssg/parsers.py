1587aaadb6215e45b4b7aa7987da6a4eaae4e7d4
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-03-31 11:06:33 -0700

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -15,4 +15,4 @@ class Parser:
             return file.read()
 
     def write(self, path, dest, content, ext = ".html"):
-        full_path= self.dest / path.with_suffix(ext).name
+        full_path= dest / path.with_suffix(ext).name
