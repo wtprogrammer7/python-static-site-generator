bd5b98796aeda8c3da63bf81e6b014eec13b0b80
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-01 11:51:37 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -13,6 +13,8 @@ class Site():
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
     def load_parser(self, extension):
         for parser in self.parsers:
             if parser.valid_extension(extension):
