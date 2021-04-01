bf65dc0538efd0bca57db2deb90ec30742a7df86
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-01 11:37:38 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -17,3 +17,5 @@ class Site():
         for parser in self.parsers:
             if parser.valid_extension(extension):
                 return parser
+    def run_parser(self, path):
+        parser = path.suffix.load_parser()
