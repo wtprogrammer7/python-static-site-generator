56b9752c30559d638c6e2775674c6ff26332d0ce
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-01 11:54:31 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -8,13 +8,6 @@ class Site():
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents = True, exist_ok = True)
-    def build(self):
-        self.dest.mkdir(parents = True, exist_ok= True)
-        for path in self.source.rglob("*"):
-            if path.is_dir():
-                self.create_dir(path)
-            elif path.is_file():
-                self.run_parser(path)
     def load_parser(self, extension):
         for parser in self.parsers:
             if parser.valid_extension(extension):
@@ -24,4 +17,13 @@ class Site():
         if parser != None:
             self.parse(parser, source, dest)
         else:
-            print("Not Implemented")
+            print("Not Implemented")    
+    
+    def build(self):
+        self.dest.mkdir(parents = True, exist_ok= True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+    
