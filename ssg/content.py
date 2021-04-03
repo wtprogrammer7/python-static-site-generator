fd6bfaacfe2851a8f07202b08551f226c247ec5c
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-03 10:12:26 -0700

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -3,5 +3,5 @@ from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
+    __delimiter = "^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
