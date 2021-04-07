94785f246735f9ec0b2d774e3198145452480b7e
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-07 09:47:38 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 
 class Site():
     def __init__(self, source, dest, parsers = None):
@@ -26,4 +27,7 @@ class Site():
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
-    
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n").format(message)
