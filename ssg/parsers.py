ab1979810fb9393712f29ef2fafe7b8ad7ccdd7a
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-07 09:35:21 -0700

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -40,3 +40,6 @@ class MarkdownParser(Parser):
         html = markdown(content.body)
         self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
+
+class RestructuredTextParser(Parser):
+    extensions = [".rst"]
