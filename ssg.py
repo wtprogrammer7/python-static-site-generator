fe3f95da4675afbec3ab8e72dc51ca4f077ec05a
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-07 09:42:52 -0700

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -6,7 +6,7 @@ def main(source = "content", dest = "dist"):
     config = {
         "source": source,
         "dest": dest,
-        "parsers": [ssg.parsers.ResourceParser()]
+        "parsers": [ssg.parsers.ResourceParser(), ssg.parsers.MarkdownParser(), ssg.parsers.ReStructuredTextParser()]
     }
     Site(**config).build()
 
