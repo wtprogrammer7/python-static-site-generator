4e0c2dc569e127865248bd7754ef5d3f5ebac9f6
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-07 09:40:10 -0700

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -39,9 +39,12 @@ class MarkdownParser(Parser):
         content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path, dest, html)
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 class ReStructuredTextParser(Parser):
     extensions = [".rst"]
     def parse(self, path: Path, source: Path, dest: Path):
         content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name = "html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
